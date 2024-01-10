3f5ca135b2b38ac19f42a86ccc1104520caa602c
Bhavesh-0909 <bhaveshchoudhary0909@gmail.com>
2024-01-10 20:22:02 +0530

    updateTodo api code

--- controller/getTodo.js
+++ controller/getTodo.js
@@ -18,3 +18,21 @@ exports.getTodos = async(req, res) => {
         })
     }
 }
+
+exports.getTodo = async(req, res)=>{
+    try{
+        const id = req.params.id;
+        const todo = await Todo.findById({_id:id})
+        res.status(200)
+        .json({
+            data:todo,
+        })
+    }
+    catch(err){
+        res.status(500)
+        .json({
+            success:false,
+            data:err.message,
+        })
+    }
+}
--- router/Todo.js
+++ router/Todo.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const {createTodo}= require('../controller/createTodo');
-const {getTodos} = require('../controller/getTodo')
+const {getTodos} = require('../controller/getTodo');
+const {getTodo} = require('../controller/getTodo');
+const {updateTodo} = require('../controller/updateTodo');
 
 router.post('/createTodo', createTodo);
 router.get('/getTodos', getTodos);
+router.get('/getTodo/:id', getTodo);
+router.put('/updateTodo/:id', updateTodo);
 
 module.exports = router;
